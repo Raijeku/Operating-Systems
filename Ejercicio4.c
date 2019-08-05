d44492c8e8459fd247bbca4864a8be7bc963a47b
Raijeku <raijeku@gmail.com>
2019-08-05 12:05:41 -0500

    Corrección Ejercicio4

--- Ejercicio4.c
+++ Ejercicio4.c
@@ -15,20 +15,22 @@ int main(){
         // child (new process)
         printf("hello, I am child (pid:%d)\n", (int) getpid());
         execl("/bin/ls", "ls", "-l", (char *)0);
-        execle("/bin/ls", "ls", "-l", (char *)0);
-        execlp("/bin/ls", "ls", "-l", (char *)0);
-        execv("/bin/ls", "ls", "-l", (char *)0);
-        execvp("/bin/ls", "ls", "-l", (char *)0);
-        execvpe("/bin/ls", "ls", "-l", (char *)0);
+        execle("/bin/ls", "ls", "-l", (char *)0, NULL);
+        execlp("ls", "ls", "-l", (char *)0);
+        char *args[]={"/bin/ls", "ls", "-l", (char *)0};
+        char *args1[]={"/bin/ls", "ls", "-l", (char *)0,NULL};
+        execv(args[0],args);
+        execvp(args[0],args);
     } else {
         // parent goes down this path (original process)
         printf("hello, I am parent of %d (pid:%d)\n",
             rc, (int) getpid());
         execl("/bin/ls", "ls", "-l", (char *)0);
-        execle("/bin/ls", "ls", "-l", (char *)0);
-        execlp("/bin/ls", "ls", "-l", (char *)0);
-        execv("/bin/ls", "ls", "-l", (char *)0);
-        execvp("/bin/ls", "ls", "-l", (char *)0);
-        execvpe("/bin/ls", "ls", "-l", (char *)0);
+        execle("/bin/ls", "ls", "-l", (char *)0, NULL);
+        execlp("ls", "ls", "-l", (char *)0);
+        char *args[]={"/bin/ls", "ls", "-l", (char *)0};
+        char *args1[]={"/bin/ls", "ls", "-l", (char *)0,NULL};
+        execv(args[0],args);
+        execvp(args[0],args);
     }
 }
